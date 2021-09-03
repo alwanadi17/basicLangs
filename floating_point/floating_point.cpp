dac67b2b5d1e67b23e9394737ddae10ddcabd14c
alpha-ai <85019398+alpha-ai@users.noreply.github.com>
2021-09-03 18:34:20 +0700

    Add unresolved floating point
    Friday, 3 September 2021. Unresolved floating point.
    
    This program worked as how floating point works. But, this isn't efficient for clearing this Kata(https://www.codewars.com/kata/5efcaedf95d7110017896ced/train/cpp).
    Written on C++. Unsolved for exponent.
    
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

---
+++ floating_point/floating_point.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+char sign(float &f)
+{
+	return f < 0 ? '1' : '0';
+}
+
+string exponent(float &f)
+{
+	return 0;
+}
+
+string binary(int &a)
+{
+	string ret, temp;
+	int num = a;
+
+	while (num != 0) {
+		temp += num%2+48;
+		num /= 2;
+	}
+	int len = temp.length();
+
+	for (int i = len-1; i >= 0; i--) ret += temp[i];
+
+	return ret;
+}
+
+string binfloat(float &f)
+{
+	string ret;
+	float num = f, sum = 0, shift = 1;
+
+	if (f < 0) num *= -1;
+
+	while (!((num-sum) < 0.0000001 && (num-sum) > -0.0000001)){
+		if ((sum+shift) > num) ret += '0';
+		else {
+			ret += '1';
+			sum += shift;
+		}
+		cout << sum << " | " << shift << " | " << ret << endl;
+		shift /= 2;
+	}
+
+	return ret;
+}
+
+
+int main(){
+	cout << "\n==============================\n";
+	cout << "FLOATING POINT";
+	cout << "\n==============================\n\n";
+
+	float fl = 0.1f, flt = fl;
+	int integer = 0;
+	string bin, temp;
+	cout << "Var fl = " << fl << "\n";
+
+	while (flt >= 1 || flt <= -1) {
+		if (flt > 0) flt--;
+		else flt++;
+		integer++;
+	}
+
+	temp = binary(integer);
+	bin += temp;
+	cout << "\n\nBinary = " << temp << "\n\n";
+	temp = binfloat(flt);
+	bin += temp;
+	cout << "\nBinary Float = " << temp << "\n\n";
+	cout << "\nUnormalized Mantissa = " << bin << "\n\n";
+
+	char s = sign(fl);
+	string exp = "";
+	string mts = "";
+
+	int len = bin.length();
+	for (int i = 1; i < len; i++) mts += bin[i];
+
+	int expo = 127;
+	temp = binary(expo);
+	len = temp.length();
+	while (len++ < 8) exp += '0';
+	exp += temp;
+
+	cout << "IEEE 754 has 3 basic component\n";
+	cout << "Sign(fl) = " << s << "\n";
+	cout << "Exponent(fl) = " << exp << "\n";
+	cout << "Mantissa(fl) = " << mts << "\n";
+
+	return 0;
+}
