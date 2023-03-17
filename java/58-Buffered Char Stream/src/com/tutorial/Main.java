b74164e21fd8c3915802037ffbd03a9d4969a648
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2023-03-17 19:28:23 +0700

    buffered stream java

--- java/58-Buffered Char Stream/src/com/tutorial/Main.java
+++ java/58-Buffered Char Stream/src/com/tutorial/Main.java
@@ -1,7 +1,44 @@
 package com.tutorial;
 
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        //open and set mark file
+        FileReader charIn = new FileReader("input.txt");
+        BufferedReader buffIn = new BufferedReader(charIn);
+
+        buffIn.mark(200);
+
+        //readline file
+        System.out.println(buffIn.readLine());
+        System.out.println(buffIn.readLine());
+        buffIn.reset();
+
+        //open output file
+        FileWriter charOut = new FileWriter("output.txt");
+        BufferedWriter buffOut = new BufferedWriter(charOut);
+
+        //write to output
+        System.out.println();
+        String tmp = buffIn.readLine();
+        System.out.println("Push \"" + tmp + "\" to buffer");
+        buffOut.write(tmp,0,tmp.length());
+        buffOut.newLine();
+
+        tmp = buffIn.readLine();
+        System.out.println("Push \"" + tmp + "\" to buffer");
+        buffOut.write(tmp,0,tmp.length());
+        buffOut.newLine();
+
+        buffOut.flush();
+
+        //close files
+        charIn.close();
+        charOut.close();
+        buffIn.close();
+        buffOut.close();
+
+
     }
 }
