416390b152f3f4281965f93e5f154980e2c5533c
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2023-03-17 18:40:12 +0700

    dummy commit, trying gpg signing key
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

--- java/58-Buffered Char Stream/src/com/tutorial/Main.java
+++ java/58-Buffered Char Stream/src/com/tutorial/Main.java
@@ -2,10 +2,6 @@ package com.tutorial;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("baris 1: using println");
-        System.out.print("baris 2: using print");
-        System.out.println("baris 3: using println");
-        System.out.print("baris 4: using newline(\\n)\n");
-        System.out.printf("baris %d: using format specifier(printf)", 5);
+        
     }
 }
