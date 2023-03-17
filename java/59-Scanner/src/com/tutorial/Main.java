f86bcc413cb2dbccfd5d255b408a98b47b5b35e0
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2023-03-17 20:39:40 +0700

    scanner and stringTokenizer java

--- java/59-Scanner/src/com/tutorial/Main.java
+++ java/59-Scanner/src/com/tutorial/Main.java
@@ -1,11 +1,44 @@
 package com.tutorial;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("baris 1: using println");
-        System.out.print("baris 2: using print");
-        System.out.println("baris 3: using println");
-        System.out.print("baris 4: using newline(\\n)\n");
-        System.out.printf("baris %d: using format specifier(printf)", 5);
+    public static void main(String[] args) throws IOException {
+        FileReader fileIn = new FileReader("input.txt");
+        BufferedReader buffIn = new BufferedReader(fileIn);
+        buffIn.mark(200);
+
+        Scanner in = new Scanner(buffIn);
+        in.useDelimiter(",");
+
+        while (in.hasNext()) {
+            System.out.println(in.next());
+        }
+
+
+        //separate all word with string tokenizer
+        System.out.println();
+        System.out.println();
+        buffIn.reset();
+        String str = buffIn.readLine();
+        System.out.println("str = " + str);
+
+        StringTokenizer strToken = new StringTokenizer(str);
+        System.out.println("strToken = " + strToken.countTokens());
+
+        while (strToken.hasMoreTokens()) {
+            System.out.println(strToken.nextToken()); //default delimiter is space
+        }
+
+        str = buffIn.readLine();
+        strToken = new StringTokenizer(str);
+
+        while (strToken.hasMoreTokens()) {
+            System.out.println(strToken.nextToken(","));
+        }
     }
 }
