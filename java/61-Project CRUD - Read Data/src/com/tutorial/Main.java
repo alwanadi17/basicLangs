29de96da911f9d8bdd2fbfa1982e4e7e7fcf4ba7
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2023-03-28 16:39:26 +0700

    CRUD Print data

--- java/61-Project CRUD - Read Data/src/com/tutorial/Main.java
+++ java/61-Project CRUD - Read Data/src/com/tutorial/Main.java
@@ -1,7 +1,10 @@
 package com.tutorial;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,56 +19,94 @@ public class Main {
             System.out.println("3.\tTambah data buku");
             System.out.println("4.\tUbah data buku");
             System.out.println("5.\tHapus data buku");
+            System.out.println("0.\tKeluar Program");
 
             System.out.print("\nPilihan anda: ");
             userInput = menuInput.next();
 
             switch (userInput) {
-                case "1":
+                case "1" -> {
                     System.out.println("\n=================");
                     System.out.println("LIST SELURUH BUKU");
                     System.out.println("=================");
                     //tampilkan data
-                    printBooks();
-                    break;
-                case "2":
+                    try {
+                        printBooks();
+                    } catch (IOException e) {
+                        System.err.println("\nDatabase tidak ditemukan");
+                        System.err.println(e);
+                    }
+                }
+                case "2" -> {
                     System.out.println("\n=========");
                     System.out.println("CARI BUKU");
                     System.out.println("=========");
-                    //cari data
-                    break;
-                case "3":
+                }
+                //cari data
+                case "3" -> {
                     System.out.println("\n================");
                     System.out.println("TAMBAH DATA BUKU");
                     System.out.println("================");
                     //tambah data
                     addBook();
-                    break;
-                case "4":
+                }
+                case "4" -> {
                     System.out.println("\n==============");
                     System.out.println("UBAH DATA BUKU");
                     System.out.println("==============");
-                    //ubah data
-                    break;
-                case "5":
+                }
+                //ubah data
+                case "5" -> {
                     System.out.println("\n===============");
                     System.out.println("HAPUS DATA BUKU");
                     System.out.println("===============");
-                    //hapus data
-                    break;
-                default:
+                }
+                //hapus data
+                case "0" -> {
+                    System.out.println("Keluar program");
+                    return;
+                }
+                default -> {
                     System.err.println("\nInput tidak ditemukan" +
                             "\nSilahkan pilih [1-5]");
                     continue;
+                }
             }
 
             isContinue = getYesNo("Apakah anda ingin melanjutkan");
         }
     }
 
-    private static void printBooks() {
-        System.out.println("we will do something cool here");
+    private static void printBooks() throws IOException {
+        FileReader dataIn;
+        BufferedReader reader;
 
+        dataIn = new FileReader("database.txt");
+        reader = new BufferedReader(dataIn);
+
+        System.out.printf("\n| %2s |\t%-7s |\t%-20s |\t%-20s |\tJudul Buku\n", "No", "Tahun", "Penulis", "Publisher");
+        System.out.println("----------------------------------------------------------------------------------------");
+
+        String line = reader.readLine();
+        StringTokenizer strLine;
+        int index = 0;
+        while (line != null) {
+            strLine = new StringTokenizer(line, ",");
+            strLine.nextToken();
+            System.out.printf("| %02d ", ++index); //no
+            System.out.printf("|\t%-7s ", strLine.nextToken()); //tahun
+            System.out.printf("|\t%-20s ", strLine.nextToken()); //penulis
+            System.out.printf("|\t%-20s ", strLine.nextToken()); //publisher
+            System.out.printf("|\t%s\n", strLine.nextToken()); //judul
+
+            line = reader.readLine();
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------");
+
+
+        dataIn.close();
+        reader.close();
     }
 
     private static void addBook() {
